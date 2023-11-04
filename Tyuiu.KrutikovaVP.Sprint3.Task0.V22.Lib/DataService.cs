415aaf1fe0447d2227d4c99c31a07b9cafeda8d4
Tyuiu.KrutikovaVP@outlook.com <lera.krutikova.2005@mail.ru>
2023-11-04 18:51:48 +0500

    Task0

---
+++ Tyuiu.KrutikovaVP.Sprint3.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KrutikovaVP.Sprint3.Task0.V22.Lib
+{
+    public class DataService : ISprint3Task0V22
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double multiplySeries = 1;
+            for (int i = 1; i<=8; i++)
+            {
+                multiplySeries *= (Math.Pow(value, i) + 4) * Math.Cos(value);
+            }
+            return Math.Round(multiplySeries,3);
+        }
+    }
+}
---
+++ Tyuiu.KrutikovaVP.Sprint3.Task0.V22/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KrutikovaVP.Sprint3.Task0.V22.Lib;
+
+namespace Tyuiu.KrutikovaVP.Sprint3.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = 0.25;
+            int startValue = 1; int stopValue = 8;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Крутикова В. П. | АСОиУБ-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                    *");
+            Console.WriteLine("* Задание #0                                                               *");
+            Console.WriteLine("* Вариант #22                                                              *");
+            Console.WriteLine("* Выполнил: Крутикова Валерия Павловна | АСОиУБ-23-3                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу используя цикл for,                                   *");
+            Console.WriteLine("* которая вычисляет произведение ряда по формуле, при a=0,25               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine($"Переменная A = {value}");
+            Console.WriteLine($"Старт шага = {startValue}");
+            Console.WriteLine($"Конец шага = {stopValue}");
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine($"Произведение ряда = {ds.GetMultiplySeries(value, startValue, stopValue)}");
+            Console.ReadKey();
+        }
+    }
+}
