aab5b8a105ef0c1f239d0d2543aed8ac8b7c4979
Tyuiu.KrutikovaVP@outlook.com <lera.krutikova.2005@mail.ru>
2023-11-04 21:10:58 +0500

    Task3

---
+++ Tyuiu.KrutikovaVP.Sprint3.Task3.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KrutikovaVP.Sprint3.Task3.V29.Lib;
+
+namespace Tyuiu.KrutikovaVP.Sprint3.Task3.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+            string value = "chgr vhhtg hnht";
+            char item = 'h';
+            var res = ds.DeleteCharInString(value, item);
+            var wait = "cgr vtg nt";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KrutikovaVP.Sprint3.Task3.V29/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KrutikovaVP.Sprint3.Task3.V29.Lib;
+
+namespace Tyuiu.KrutikovaVP.Sprint3.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Крутикова В. П. | АСОиУБ-23-3";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                    *");
+            Console.WriteLine("* Задание #3                                                               *");
+            Console.WriteLine("* Вариант #29                                                              *");
+            Console.WriteLine("* Выполнил: Крутикова Валерия Павловна | АСОиУБ-23-3                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Используя цикл foreach удалить из строки все буквы h                     *");
+            Console.WriteLine("* В строке: chgr vhhtg hnht                                                *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            string value = "chgr vhhtg hnht";
+            char chr = 'h';
+
+            Console.WriteLine($"Исходная строка = {value}");
+            Console.WriteLine($"Исходный символ = {chr}");
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine($"Конечная строка = {ds.DeleteCharInString(value, chr)}");
+            Console.ReadKey();
+        }
+    }
+}
